e7d37ad2709400c637e228bdd1a9e3ca2b13da96
Ntandoyenkosi Xakaza <xakazantando@gmail.com>
2026-02-19 20:55:31 +0200

    Changing style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -29,7 +29,7 @@ export default function App() {
 
   // Show main website
   return (
-    <div className="min-h-screen bg-white">
+    <div>
       <Header onBookNow={handleBookNow} />
       <Hero onBookNow={handleBookNow} />
       <Services onBookNow={handleBookNow} />
